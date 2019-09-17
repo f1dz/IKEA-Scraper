84946e0f6f19a6e068c0d9fbe00822f79168d3bd
Khofidin <offiedz@gmail.com>
2019-09-17 08:54:14 +0700

    Product Update. Change Category input to Select2

--- views/product/_form.php
+++ views/product/_form.php
@@ -2,10 +2,13 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\web\JsExpression;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Product */
 /* @var $form yii\widgets\ActiveForm */
+
+$url = \yii\helpers\Url::to(['/category/list'])
 ?>
 
 <div class="product-form">
@@ -20,7 +23,27 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'price_profit')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'category_id')->textInput() ?>
+    <?= $form->field($model, 'category_id')->widget(\kartik\select2\Select2::class, [
+        'initValueText' => @$model->category->full_text,
+        'value' => @$model->category_id,
+        'theme' => \kartik\select2\Select2::THEME_BOOTSTRAP,
+        'options' => ['placeholder' => 'Search for a port ...'],
+        'pluginOptions' => [
+            'allowClear' => true,
+            'minimumInputLength' => 3,
+            'language' => [
+                'errorLoading' => new JsExpression("function () { return 'Waiting for results...'; }"),
+            ],
+            'ajax' => [
+                'url' => $url,
+                'dataType' => 'json',
+                'data' => new JsExpression('function(params) { return {q:params.term}; }')
+            ],
+            'escapeMarkup' => new JsExpression('function (markup) { return markup; }'),
+            'templateResult' => new JsExpression('function(category_id) { return category_id.text; }'),
+            'templateSelection' => new JsExpression('function (category_id) { return category_id.text; }'),
+        ]
+    ]) ?>
 
     <?= $form->field($model, 'article_no')->textInput(['maxlength' => true]) ?>
 
